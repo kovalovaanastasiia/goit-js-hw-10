d9d1eb05b7ac1e3216a4fcffe141b2f8b4d8dc74
kovalovaanastasiia <tokach26@gmail.com>
2023-03-27 20:08:04 +0300

    file index.js was changed.

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector("#search-box");
+const countryListEl = document.querySelector('.country-list');
+const countryDivEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
 function inputHandler() {
   let countryName = inputEl.value.trim()
-  fetchCountries(countryName)
-    .then(dataCountries => {
-      makeMarcupCountries(dataCountries)
-    })
-    .catch(error => {
-      Notify.failure('Oops, there is no country with that name');
-    })
+  if (countryName) {
+    fetchCountries(countryName)
+      .then(dataCountries => {
+        makeMarcupCountries(dataCountries)
+      })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      })
+  } else {
+    countryListEl.innerHTML = "";
+    countryDivEl.innerHTML = "";
+  }
+
 }
